4d7f185296754229f2939a9453be7ae8afcd027b
ANBARASU MANI <arasuamie@gmail.com>
2023-08-09 22:09:36 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+console.log('Clinet side javascript file is loaded!')
+
+
+// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+//       response.json().then((data)=>{
+//           console.log(data)
+//       })
+// })
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector ('input')
+const messageOne = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
+
+weatherForm.addEventListener('submit', (e) =>{
+   e.preventDefault()
+   
+   const location = search.value
+
+   messageOne.textContent = 'Loading..'
+   message2.textContent =''
+
+   fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+      response.json().then((data)=>{
+         if (data.error){
+           
+            messageOne.textContent = data.error
+         
+         } else { 
+            messageOne.textContent = data.location
+            message2.textContent = data.forecast
+            }
+                  
+        })
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require ('request')
+
+
+const forecast = (latitude, longitude, callback) => {
+
+const url = 'http://api.weatherapi.com/v1/current.json?key=88ab2555b17c45e28ce155349230507&q=' + latitude + ',' + longitude
+
+request({ url , json:true }, (error, {body}) => {
+    if (error){
+              callback('Unable to connect to the Weather service!', undefined)
+    } else if (body.error) {
+              callback('Unable to find the location', undefined)
+   } else
+           callback( undefined, body.current.condition.text + '. it is currently ' +  body.current.temp_c  + ' degrees_out'+ ' It feels like a ' +  body.current.feelslike_c + ' degrees' )
+})
+}
+
+module.exports= forecast
